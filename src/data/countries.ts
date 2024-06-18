64d09b6f9e351c94ece22905a773a42b85ded0f0
marco-soria <marco.soriaa7@gmail.com>
2024-06-18 11:02:42 -0500

    first commit

---
+++ src/data/countries.ts
@@ -0,0 +1,11 @@
+import type { Country } from "../types"
+
+export const countries : Country[] = [
+    { code: 'US', name: 'USA' },
+    { code: 'MX', name: 'Mexico' },
+    { code: 'AR', name: 'Argentina' },
+    { code: 'CO', name: 'Colombia' },
+    { code: 'CR', name: 'Costa Rica' },
+    { code: 'ES', name: 'Spain' },
+    { code: 'PE', name: 'Peru' },
+]
